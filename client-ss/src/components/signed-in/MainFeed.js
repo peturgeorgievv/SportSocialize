bb1ff53d2048af93717a65db83d95e3f36e95ba6
Петър Георгиев <peturgeorgievv@gmail.com>
2019-09-28 21:34:28 +0300

    Adding state for all posts from all users in main feed

--- client-ss/src/components/signed-in/MainFeed.js
+++ client-ss/src/components/signed-in/MainFeed.js
@@ -1,78 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import $http from '../../api/users';
 // Have to change user-pic className to unique ID of the user
 
 class MainFeed extends React.Component {
+  state = { data: [] };
+
+  async componentDidMount() {
+    const token = localStorage.getItem('currentUser');
+    const response = await $http.get(`/api/users/posts`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+
+    this.setState({ data: response.data });
+    console.log(this.state.data);
+  }
+
+
+
+
   render() {
+
+    const allImages = this.state.data.map((image) => {
+      return (
+        <div>
+          <div className="card-deck">
+            <div className="card">
+              <img src={image.photoUrl} alt={image.title} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{image.title}</h5>
+                <p className="card-text">{image.description}</p>
+                <p className="card-text"><Link className="profile-link" to="/logged/user">
+                  Petar Georgiev Georgiev
+                </Link></p>
+              </div>
+            </div>
+          </div><br />
+        </div>
+      )
+    })
+
+
     return (
       <div className="container">
 
-        <div className="card-deck">
-          <div className="card">
-            <img src={require('./imgs/MikeTyson1.jpg')} alt="MikeTyson" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-          <div className="card">
-            <img src={require('./imgs/Tyson2.jpg')} alt="MikeTyson2" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-          <div className="card">
-            <img src={require('./imgs/TysonFury.jpg')} alt="Fury" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-
-        </div><br />
-
-        <div className="card-deck">
-          <div className="card">
-            <img src={require('./imgs/MikeTyson1.jpg')} alt="MikeTyson" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-          <div className="card">
-            <img src={require('./imgs/Tyson2.jpg')} alt="MikeTyson2" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-          <div className="card">
-            <img src={require('./imgs/TysonFury.jpg')} alt="Fury" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Some post</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p className="card-text"><Link className="profile-link" to="/logged/user">
-                Petar Georgiev Georgiev
-                  </Link></p>
-            </div>
-          </div>
-        </div>
+        {allImages}
 
       </div>
     );
